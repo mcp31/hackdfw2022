e8df3e3883955d24b83ff7fad874349540612e18
mcp31 <mcplana31@gmail.com>
2022-09-24 02:15:11 -0500

    made skeleton layout

--- pages/homeScreen.js
+++ pages/homeScreen.js
@@ -1,5 +1,48 @@
 import styles from '../styles/Login.module.css'
 
 export default function HomeScreen() {
+    return (
+        <div>
+            <div>
+                <h1>
+                    Blueberry Logo
+                </h1>
+            </div>
+            <div>
+                <h1>
+                    Time Remaining
+                </h1>
+                <h3>
+                    Progress Bar
+                </h3>
+            </div>
+            <div>
+                <h1>
+                    Budget
+                </h1>
+                <h3>
+                    Progress Bar
+                </h3>
+            </div>
+            <div>
+                <h1>
+                    Your Buckets
+                </h1>
+                <h3>
+                    List of Buckets
+                </h3>
+            </div>
+            <div>
+                <h1>
+                    Transactions
+                </h1>
+                <h3>
+                    List of Transactions
+                </h3>
+            </div>
+        </div>
+        
+        
 
+    )
 }
