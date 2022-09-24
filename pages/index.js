2edbf8774572f668f5c9c9550361d46c8b2fc7a2
Peyton T <peyton.t131@gmail.com>
2022-09-24 13:59:51 -0500

    almost fix

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,30 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import HomeScreen from './homeScreen'
+var TodoBudget = require('todo_budget');
+
+// The following property can be used to configure cross-origin resource sharing
+// in the HTTP nodes.
+// See https://github.com/troygoode/node-cors#configuration-options for
+// details on its contents. The following is a basic permissive set of options:
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <HomeScreen></HomeScreen>
-    </div>
-  )
+
+    let apiInstance = new TodoBudget.DefaultApi();
+    let opts = {
+        'limit': 56 // Number | maximum number of results to return
+    };
+    apiInstance.findUsers(opts, (error, data, response) => {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        }
+    });
+
+    return ( < div className = { styles.container } >
+        <
+        HomeScreen > < /HomeScreen> </div >
+    )
+
 }
